d43f34a13ad46d460eb18c39a9abaeb8376ade13
fmallick <33433462+fsmallick@users.noreply.github.com>
2020-04-10 17:13:18 -0400

    Added profile page

---
+++ app/src/main/java/com/example/a434_health_app/Profile_Activity.java
@@ -0,0 +1,16 @@
+package com.example.a434_health_app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Profile_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+
+}
