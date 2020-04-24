046870c40f9629c11f661880157c64860b92c144
Matthew Aird <aird.matthew@gmail.com>
2020-04-24 05:06:38 -0400

    updated goals and types of goals page

--- app/src/main/java/com/example/a434_health_app/Goals_Activity.java
+++ app/src/main/java/com/example/a434_health_app/Goals_Activity.java
@@ -1,6 +1,9 @@
 package com.example.a434_health_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -25,6 +28,18 @@ public class Goals_Activity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+
+
+        Button addGoalsButton = findViewById(R.id.adds_goals);
+
+        addGoalsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+                startActivity(new Intent(getApplicationContext(), activity_types_of_goals.class));
+            }
+        });
+
     }
 
 }
---
+++ app/src/main/java/com/example/a434_health_app/activity_types_of_goals.java
@@ -0,0 +1,18 @@
+package com.example.a434_health_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class activity_types_of_goals extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_types_of_goals);
+    }
+
+}
