e29f905faeeed15e00b15e030c70b5a9d2110d1e
katherinesebina <katherinesebina@gmail.com>
2020-04-24 04:06:47 -0400

    flash screen java file

--- app/src/main/java/com/example/a434_health_app/MainActivity.java
+++ app/src/main/java/com/example/a434_health_app/MainActivity.java
@@ -4,24 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button goalsButton = findViewById(R.id.goalsButton);
-
-        goalsButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                System.out.println("Button Clicked");
-                startActivity(new Intent(getApplicationContext(), Goals_Activity.class));
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
             }
-        });
+        }, SPLASH_TIME_OUT);
+
     }
 
 }
