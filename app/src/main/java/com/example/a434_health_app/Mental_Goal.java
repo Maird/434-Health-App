fb32f2adbf6e920e09f89282ab91e3cda9220858
Matthew Aird <aird.matthew@gmail.com>
2020-04-24 18:10:20 -0400

    Added Mental health goals page and added color styles

---
+++ app/src/main/java/com/example/a434_health_app/Mental_Goal.java
@@ -0,0 +1,14 @@
+package com.example.a434_health_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mental_Goal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mental__goal);
+    }
+}
--- app/src/main/java/com/example/a434_health_app/activity_types_of_goals.java
+++ app/src/main/java/com/example/a434_health_app/activity_types_of_goals.java
@@ -1,11 +1,12 @@
 package com.example.a434_health_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class activity_types_of_goals extends AppCompatActivity {
 
@@ -13,6 +14,20 @@ public class activity_types_of_goals extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_types_of_goals);
+
+
+
+
+        Button addGoalsButton = findViewById(R.id.mental_goal);
+
+        addGoalsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+                startActivity(new Intent(getApplicationContext(), Mental_Goal.class));
+            }
+        });
     }
+
+
 
 }
