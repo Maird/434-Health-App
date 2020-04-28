922162c5e337968ecdeebd9b32b9ec0f78018f37
Matthew Aird <aird.matthew@gmail.com>
2020-04-27 23:26:56 -0400

    Fixed Profile page

--- app/src/main/java/com/example/a434_health_app/HomeActivity.java
+++ app/src/main/java/com/example/a434_health_app/HomeActivity.java
@@ -13,6 +13,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_home);
 
         Button goalsButton = findViewById(R.id.goalsButton);
