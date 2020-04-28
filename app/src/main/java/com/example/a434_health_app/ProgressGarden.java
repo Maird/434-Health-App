a596e08f4f8dc936ea22fe726809ff32739c9cf8
katherinesebina <katherinesebina@gmail.com>
2020-04-28 02:58:28 -0400

    add back button

--- app/src/main/java/com/example/a434_health_app/ProgressGarden.java
+++ app/src/main/java/com/example/a434_health_app/ProgressGarden.java
@@ -3,12 +3,22 @@ package com.example.a434_health_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProgressGarden extends AppCompatActivity {
 
+    private Button goalsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress_garden);
+
+        goalsButton = findViewById(R.id.goalsButton);
+        goalsButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
